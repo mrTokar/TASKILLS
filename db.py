2e3545e4c743004bf70c90ebc7e76e5fc6f11e91
Tokar <egortok2014@gmail.com>
2021-11-16 22:16:29 +0300

    Добавление коментариев для функций

--- db.py
+++ db.py
@@ -5,7 +5,8 @@ conn = sqlite3.connect('db/dnd_db.db', check_same_thread=False) # ...=False --> 
 cursor = conn.cursor()
 
 
-def get_setting(mapn):
+def get_setting(mapn) -> str:
+    """Возвращает сеттинг по мапу"""
     conn = sqlite3.connect('db/dnd_db.db', check_same_thread=False) # ...=False --> don't while check
     cursor = conn.cursor()
     cursor.execute("select setting from set_map where maps=:mapn", {"mapn": mapn})
@@ -13,17 +14,19 @@ def get_setting(mapn):
     return setting[0][0]
 
 
-def get_maps(setting):
+def get_maps(setting) -> str:
+    '''Возвращает мапы по сеттингу sep=";"'''
     conn = sqlite3.connect('db/dnd_db.db', check_same_thread=False) # ...=False --> don't while check
     cursor = conn.cursor()
     cursor.execute("select maps from set_map where setting=:setting", {"setting": setting})
     maps = cursor.fetchall()
     maps = [x[0] for x in maps]
-    maps = ', '.join(maps)
+    maps = ';'.join(maps)
     return maps
 
 
-def get_info(mapn):
+def get_info(mapn) -> str:
+    """Возвращает информацию о локации и сеттинге"""
     conn = sqlite3.connect('db/dnd_db.db', check_same_thread=False) # ...=False --> don't while check
     cursor = conn.cursor()
     cursor.execute("select maps_info from set_map where maps=:mapn", {"mapn": mapn})
