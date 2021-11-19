4ae5d598940d8e87b99a6d95ae18c79cba810ab3
Tokar <egortok2014@gmail.com>
2021-11-19 23:32:15 +0300

    Update function's returns
    Адапитировны для выводя информации  в GUI

--- get_any.py
+++ get_any.py
@@ -5,24 +5,25 @@ conn = sqlite3.connect('db/info_settings.db', check_same_thread=False) # ...=Fal
 cursor = conn.cursor()
 
 
-def get_setting(mapn = 0) -> str:
-    """Возвращает сеттинг по мапу"""
-    if mapn != 0:
+def get_setting(mapn = None) -> str or list:
+    """Возвращает сеттинг по мапу.
+    Если в аргументы нчиего не передано, то функция возращает список все сеттинги"""
+    if mapn is not None:
         cursor.execute("select setting from set_map where maps=:mapn", {"mapn": mapn})
         setting = cursor.fetchall()
-        setting = setting[0][0]
+        settings = setting[0][0]
     else:
         cursor.execute("select setting from set_map")
         setting = cursor.fetchall()
-        settings = {e for l in setting for e in l}
+        settings = list({e for l in setting for e in l})
 
 
-    return settings # return in set() format str
+    return settings # return str
 
 
-def get_maps(setting = 0) -> str:
+def get_maps(setting = None) -> str:
     '''Возвращает мапы по сеттингу sep=";"'''
-    if setting != 0:
+    if setting is not None:
         cursor.execute("select maps from set_map where setting=:setting", {"setting": setting})
         maps = cursor.fetchall()
         maps = [x[0] for x in maps]
@@ -45,27 +46,28 @@ def get_maps_info(mapn) -> str:
     return info[0][0] # return in str
 
 
-def get_hero_info(hero=0):
-    if hero != 0:
+def get_hero_info(hero=None) -> str or None:
+    """Функция возращает описание неигровго персонажа.
+    Если персонажа нет в БД, функция возращает None"""
+    if hero is not None:
         try:
             cursor.execute("select hero_info from heroes_dnd where hero=:hero", {"hero": hero})
             hero_info = cursor.fetchall()
             hero_info = hero_info[0][0]
         except IndexError:
-            return 'Такого персонажа не существует.'
+            return None
     else:
         cursor.execute("SELECT hero_info FROM heroes_dnd")
         hero_info = cursor.fetchall()
         # hero_info = hero_info[0][0]
 
-    return hero_info # return in str (if hero=0)
+    return hero_info # return in str
 
+if __name__ == "__main__":
+    print('def get_setting: ', get_setting())
+    print('def get_maps: ', get_maps())
+    print('def get_item_info: ', get_maps_info('Nirn'))
+    print('def get_hero_info: ', get_hero_info('Вампиры'))
 
-'''
-print('def get_setting: ', get_setting())
-print('def get_maps: ', get_maps())
-print('def get_item_info: ', get_maps_info('Nirn'))
-print('def get_hero_info: ', get_hero_info('Вампиры'))
-'''
 
 conn.close()
